17aa05320a370108f22b0f516c80faa060cd30fa
anandmanikutty <anand.x.manikutty@gmail.com>
2017-02-22 16:20:26 -0800

    Create Lecture1__Program3.cpp

---
+++ Lecture1__Program3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //
+  // Using int Variables for Fun and Profit
+  //
+  int frances, francisco, reuben;
+
+  frances = 37;
+  francisco = 33;
+  reuben = 31;
+
+ cout << “Frances got the highest score.” << endl;
+ cout << “Congratulations, Frances!” << endl;
+
+  return 0;
+}
