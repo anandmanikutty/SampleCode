f4e3a8db345846d7b8a6970fe46a7b6849dc8ea7
anandmanikutty <anand.x.manikutty@gmail.com>
2017-02-22 16:29:01 -0800

    Create 01_Lecture2__Program1.cpp

---
+++ 01_Lecture2__Program1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ // Lecture -- review code
+ 
+ int findex;
+  
+ cout << "Enter the value of findex:";
+ cin >> findex;
+ cout << "findex = " << findex << endl;
+
+  return 0;
+}
