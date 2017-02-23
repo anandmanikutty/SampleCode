14ea85cbd0341361e0c8ba429c3cdd122f12d374
anandmanikutty <anand.x.manikutty@gmail.com>
2017-02-22 16:34:44 -0800

    Create 01_Lecture2__Program3.cpp

---
+++ 01_Lecture2__Program3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int seconds, minutes;
+   
+   seconds = 240;
+   
+   minutes = seconds/60;
+   
+   cout << "Time is : " << minutes << " minutes." << endl;
+   
+   return 0;
+}
