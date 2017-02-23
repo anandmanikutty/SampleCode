a123177411f3b3c280b22463ecd9818f042f4d6b
anandmanikutty <anand.x.manikutty@gmail.com>
2017-02-22 16:20:49 -0800

    Create Lecture1__Program4.cpp

---
+++ Lecture1__Program4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //
+  // User Input and Output Using cin & cout
+  //
+  int frances, francisco, reuben;
+
+  frances = 37;
+  francisco = 33;
+  reuben = 31;
+
+ cout << “Frances got the highest score.” << endl;
+ cout << “Congratulations, Frances!” << endl;
+
+ int totalScore;
+ cout << “Enter the value of the total score the exam is out of:”;
+ cin >> totalScore;
+ Cout << “The total score was : “ << totalScore << endl;
+
+  return 0;
+}
