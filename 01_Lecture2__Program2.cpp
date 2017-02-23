e422ea9648d43ea17ee23140db68bd7021d7fb4e
anandmanikutty <anand.x.manikutty@gmail.com>
2017-02-22 16:33:14 -0800

    Create 01_Lecture2__Program2.cpp

---
+++ 01_Lecture2__Program2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //
+  // Declaring variables
+  //
+  int totalScore1, totalScore2;
+  
+  cout << "Enter the first two total scores: " << endl;
+  // TOTAL SCORE 1
+  cout << "(1) First total score: " << endl;
+  cin >> totalScore1;
+  
+  // TOTAL SCORE 2
+  cout << "(2) Second total score: " << endl;
+  cin >> totalScore2;
+ 
+  return 0;
+}
