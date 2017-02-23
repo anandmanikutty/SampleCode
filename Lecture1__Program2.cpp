b7bc647a9487abc1be905f5e364569d50838f78e
anandmanikutty <anand.x.manikutty@gmail.com>
2017-02-22 16:19:59 -0800

    Create Lecture1__Program2.cpp

---
+++ Lecture1__Program2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //
+  // Your All Purpose, Multi-Grain, Gluten-Free Program Shell
+  //
+  // Your code goes inside here
+  //
+  return 0;
+}
+
